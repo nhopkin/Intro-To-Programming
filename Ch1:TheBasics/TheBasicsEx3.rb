51e162bb0107a6011009942d8b82b020fd1ce308
Nick <nhopkin@jacksonville.edu>
2019-05-04 19:42:01 -0400

    Intro to Programming: Ch. 1 The Basics

---
+++ Ch1:TheBasics/TheBasicsEx3.rb
@@ -0,0 +1,5 @@
+my_movies = {:The_Shawshank_Redemtion => 1994, :Terminator_2 => 1991, :Crocodile_Dundee => 1986}
+
+puts my_movies[:The_Shawshank_Redemtion]
+puts my_movies[:Terminator_2]
+puts my_movies[:Crocodile_Dundee]
