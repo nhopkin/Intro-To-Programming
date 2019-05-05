6adcbe3fca120fd465ebf3f74bf7451e58cad03e
Nick <nhopkin@jacksonville.edu>
2019-05-04 23:45:35 -0400

    Intro to Programming: Variable Exercises

---
+++ Ch2:Variables/age.rb
@@ -0,0 +1,11 @@
+puts "How old are you?"
+user_age = gets.chomp.to_i 
+
+puts "In 10 years you will be:"
+puts user_age + 10
+puts "In 20 years you will be:"
+puts user_age + 20
+puts "In 30 years you will be:"
+puts user_age + 30
+puts "In 40 years you will be:"
+puts user_age + 40
---
+++ Ch2:Variables/name.rb
@@ -0,0 +1,6 @@
+puts "Please type your name: "
+user_name = gets.chomp
+
+puts "Hello, how are you #{user_name}!"
+
+10.times {puts user_name}
